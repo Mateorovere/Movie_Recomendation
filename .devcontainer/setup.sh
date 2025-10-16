4ad03a913009be22c2e961b72d99b7ec2a9f1a58
Mateorovere <roveremate@gmail.com>
2025-10-16 20:29:33 +0000

    second commit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,8 +2,7 @@
 set -e
 
 echo "Installing uv..."
-curl -LsSf https://astral.sh/uv/install.sh | sh
-export PATH="/home/vscode/.cargo/bin:$PATH"
+pip install uv pre-commit
 
 echo "Setting up Python environment with uv..."
 uv venv --clear
@@ -12,8 +11,6 @@ source .venv/bin/activate
 echo "Installing dependencies..."
 uv sync --dev
 
-
 echo "Installing pre-commit hooks..."
 pre-commit install
-
 echo "Setup complete! Your development environment is ready."
